a19f2e278429e737981ef00c1d78288911dcdd7f
Jaden Pacificar <jaden.pacificar@wvsu.edu.ph>
2024-12-03 01:17:21 +0800

    changed tab icon colors and type

--- lib/features/BoarderSideNav.dart
+++ lib/features/BoarderSideNav.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:cc_206_boarding_house_locator/features/BoarderSideTabs/home_tab.dart';
 import 'package:cc_206_boarding_house_locator/features/BoarderSideTabs/profile_tab.dart';
 import 'package:cc_206_boarding_house_locator/features/BoarderSideTabs/saved_tab.dart';
-import 'package:flutter/material.dart';
 
 class BoarderHomePage extends StatefulWidget {
   @override
@@ -11,24 +11,41 @@ class BoarderHomePage extends StatefulWidget {
 class _BoarderHomePageState extends State<BoarderHomePage> {
   int _currentIndex = 0; // to track the selected tab
 
-  //  screens for each tab
+  // Screens for each tab
   final List<Widget> _boarderScreens = [HomeTab(), SavedTab(), ProfileTab()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _boarderScreens[_currentIndex], // show the selected screen
+      body: _boarderScreens[_currentIndex], // Show the selected screen
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
+        selectedItemColor: Colors.green, // Color when selected
+        unselectedItemColor: Colors.grey, // Color when not selected
         onTap: (index) {
           setState(() {
-            _currentIndex = index; // update selected tab on tap
+            _currentIndex = index; // Update selected tab on tap
           });
         },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: 'Saved'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              _currentIndex == 0 ? Icons.home : Icons.home_outlined,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              _currentIndex == 1 ? Icons.bookmark : Icons.bookmark_border,
+            ),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              _currentIndex == 2 ? Icons.person : Icons.person_outline,
+            ),
+            label: 'Profile',
+          ),
         ],
       ),
     );
